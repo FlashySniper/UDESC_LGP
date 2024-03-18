adc41b131f27ddda28280c96df6eea08fc0c78dc
Gabriel L Soares <gabri.soares007@gmail.com>
2024-03-18 15:22:29 -0300

    Update ex005.c

--- Lista 1.4 - Funcao/ex005.c
+++ Lista 1.4 - Funcao/ex005.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int eh_primo(int k, int n);
+// int eh_primo(int k, int n);
 
-int main(int argc, char *argv[]) {
-  int k, n=0, cont=0;
-  printf("Digite o numero inicial: ");
-  scanf("%i", &k);
-  printf("Digite o numero de numeros primos que deseja saber: ");
-  scanf("%i", &n);
-  printf("Os %i numeros primos depois de %i são: ", k, n);
-  while(cont < n){
-    cont += eh_primo(k, n);
-    printf("%i", cont);
-  }
-  return 0;
-}
+// int main(int argc, char *argv[]) {
+//   int k, n=0, cont=0;
+//   printf("Digite o numero inicial: ");
+//   scanf("%i", &k);
+//   printf("Digite o numero de numeros primos que deseja saber: ");
+//   scanf("%i", &n);
+//   printf("Os %i numeros primos depois de %i são: ", k, n);
+//   while(cont < n){
+//     cont += eh_primo(k, n);
+//     printf("%i", cont);
+//   }
+//   return 0;
+// }
 
-int eh_primo(int k, int n){
-  int cont=0, resto=0, i=0;
-  for(i=0; i<= k; i++){
-    if(k%i == 0){
-      resto++;
-    }
-    if(resto == 2){
-      printf("%i", k);
-      cont++;
-    }
-  }
-  return cont;
-}
+// int eh_primo(int k, int n){
+//   int cont=0, resto=0, i=0;
+//   for(i=0; i<= k; i++){
+//     if(k%i == 0){
+//       resto++;
+//     }
+//     if(resto == 2){
+//       printf("%i", k);
+//       cont++;
+//     }
+//   }
+//   return cont;
+// }
 
 #include <stdio.h>
 
